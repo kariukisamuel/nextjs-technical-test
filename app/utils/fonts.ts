4b8c7f06433e96e01309324874dc261f704ce53d
codesniper <codesniper@localhost.localdomain>
2024-09-11 16:09:03 -0400

    Fix font.ts bug: Declare font subsets

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -2,11 +2,13 @@ import { Montserrat, Roboto } from "next/font/google"
 
 export const montserrat_init = Montserrat({
     variable: '--font-montserrat',
-    weight: '700'
+    weight: '700',
+    subsets:['latin']
 });
 export const roboto_init = Roboto({
     variable: '--font-roboto',
-    weight: '300'
+    weight: '300',
+    subsets:['latin']
 })
 
 export const montserrat = montserrat_init.variable;
