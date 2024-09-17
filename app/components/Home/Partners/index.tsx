3d370b97cbb2845a3a83ab23c02c0e34647a9ea4
codesniper <codesniper@localhost.localdomain>
2024-09-17 17:30:33 -0400

    Add Modal Component, Fix HowItWorksSection Mapping, Add SkeletonLoader, and Rename API Module

--- app/components/Home/Partners/index.tsx
+++ app/components/Home/Partners/index.tsx
@@ -1,11 +1,60 @@
+"use client";
+import React, { useState, useEffect } from 'react';
 import SectionTitle from "../../Common/SectionTitle";
 import ProfileCard from "./ProfileCard";
+import Modal from "../../Common/Modal";
+import SkeletonLoader from '../../Common/SkeletonLoader';
+import LoadMoviesAndActors from '@/app/api/LoadMoviesAndActors';
 
 type title = {
   title: string;
 };
+interface KnownForItem {
+  overview: string;
+}
+
+interface Actors {
+  id: number;
+  name: string;
+  profile_path: string;
+  known_for: KnownForItem[];
+}
 
 const Partners = ({ title }: title) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [actors, setActors] = useState<Actors[]>([]);
+  const [visibleActors, setVisibleActors] = useState<Actors[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const count = 4;
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchActors = async (limit: number) => {
+      try {
+        const response = await LoadMoviesAndActors('persons', 1);
+        const allActors: any = response.slice(0, limit);
+        setTimeout(() => {
+          setActors(allActors);
+          setVisibleActors(allActors.slice(0, count))
+          setIsLoading(false);
+        }, 15000); // 15,000 milliseconds = 15 seconds
+      } catch (error) {
+        console.error('Error fetching actors:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchActors(16);
+  }, []);
+
   return (
     <section id="meet-a-partner" className="w-full pt-20">
       <div className="w-5/6 lg:w-[70%] mx-auto">
@@ -13,21 +62,75 @@ const Partners = ({ title }: title) => {
           <SectionTitle title={title} />
         </div>
         <div className="flex justify-between mt-10 flex-wrap">
-          <ProfileCard />
-          <ProfileCard />
-          <ProfileCard />
-          <ProfileCard />
+
+          {
+            isLoading
+              ?
+              <>
+                <SkeletonLoader width="w-[100px]" height="h-[100px]" circular />
+                <SkeletonLoader width="w-[100px]" height="h-[100px]" circular />
+                <SkeletonLoader width="w-[100px]" height="h-[100px]" circular />
+                <SkeletonLoader width="w-[100px]" height="h-[100px]" circular />
+
+              </>
+
+              :
+              visibleActors.map((actor) => (
+                <ProfileCard
+                  key={actor.id}
+                  imageUrl={actor.profile_path}
+                  name={actor.name}
+                  knownFor={actor.known_for[0].overview}
+
+                />
+              ))}
+
         </div>
         <div className="flex justify-center">
-          <a
-            href="#"
+          {
+          visibleActors.length > 0 && 
+          <button
+            onClick={openModal}
             className=" bg-white text-blazingRed border-2 border-blazingRed py-2 px-6   hover:bg-blazingRed hover:text-white hover hover:border-2 hover:border-white rounded-full text-lg font-bold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
           >
             See other partners
-          </a>
+          </button>
+          }
+
+
+          <Modal isOpen={isModalOpen} onClose={closeModal}>
+            {
+              isLoading
+                ?
+                <>
+                  <SkeletonLoader width="w-full sm:w-[50%] md:w-[23.5%] lg:w-[23.5%]" height="h-[200px]" circular />
+
+                </>
+
+                :
+
+                <>
+                  <SectionTitle title="All Partners" />
+                  <div className="flex flex-wrap my-5">
+                    {actors.map((actor) => (
+                      <ProfileCard
+                        key={actor.id}
+                        imageUrl={actor.profile_path}
+                        name={actor.name}
+                        knownFor={actor.known_for[0].overview}
+                      />
+                    ))}
+                  </div>
+                </>
+
+
+
+            }
+
+          </Modal>
         </div>
       </div>
-    </section>
+    </section >
   );
 };
 
